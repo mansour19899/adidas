1cab70b7db31d2ff9c6b13a1b873658bd045e30b
mansour <smm.2020@gmail.com>
2018-04-13 10:19:44 +0430

    PersonMetaData

---
+++ Adidas/Adidas/ViewModels/PersonInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using  Adidas.Models.DomainModels;
+
+namespace Adidas.ViewModels
+{
+    public class PersonInfo
+    {
+        public Person Person { get; set; }
+        public JobRecord JobRecord1 { get; set; }
+        public JobRecord JobRecord2 { get; set; }
+        public JobRecord JobRecord3 { get; set; }
+        public RelationShip RelationShip1 { get; set; }
+        public RelationShip RelationShip2 { get; set; }
+        public RelationShip RelationShip3 { get; set; }
+        public PersonInfo()
+        {
+            
+        }
+    }
+}
