564757432c4624d1242eeeeb57036dcf0ecbad8e
mansour <smm.2020@gmail.com>
2018-06-09 00:15:35 +0430

    ..

--- Adidas/Controllers/AdminController.cs
+++ Adidas/Controllers/AdminController.cs
@@ -5,13 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Adidas.Models.DomainModels;
 
 namespace Adidas.Controllers
 {
+   
     public class AdminController : Controller
     {
+   
         // GET: Admin
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ControlPanel ()
         {
             return View();
         }
@@ -21,7 +29,7 @@ namespace Adidas.Controllers
             PersonRepository blPerson = new PersonRepository();
 
             var list = blPerson.Where(p=>p.Checked==false).ToList();
-
+         
             return View(list);
         }
 
@@ -35,25 +43,35 @@ namespace Adidas.Controllers
             PersonInfo infoo = new PersonInfo();
 
             var t = blPerson.Find(id);
-            var tt = blJob.Where(p => p.Person_FK == t.Id).ToList();
-            var ttt = blRelation.Where(p => p.Person_FK == t.Id).ToList();
-            infoo.Person = t;
+            if(t!=null)
+            {
+                var tt = blJob.Where(p => p.Person_FK == t.Id).ToList();
+                var ttt = blRelation.Where(p => p.Person_FK == t.Id).ToList();
+                infoo.Person = t;
 
-            if (tt.Count() > 0)
-                infoo.JobRecord1 = tt.ElementAt(0);
-            if (tt.Count() > 1)
-                infoo.JobRecord2 = tt.ElementAt(1);
-            if (tt.Count() > 2)
-                infoo.JobRecord3 = tt.ElementAt(2);
+                if (tt.Count() > 0)
+                    infoo.JobRecord1 = tt.ElementAt(0);
+                if (tt.Count() > 1)
+                    infoo.JobRecord2 = tt.ElementAt(1);
+                if (tt.Count() > 2)
+                    infoo.JobRecord3 = tt.ElementAt(2);
 
-            if (ttt.Count() > 0)
-                infoo.RelationShip1 = ttt.ElementAt(0);
-            if (ttt.Count() > 1)
-                infoo.RelationShip2 = ttt.ElementAt(1);
-            if (ttt.Count() > 2)
-                infoo.RelationShip3 = ttt.ElementAt(2);
+                if (ttt.Count() > 0)
+                    infoo.RelationShip1 = ttt.ElementAt(0);
+                if (ttt.Count() > 1)
+                    infoo.RelationShip2 = ttt.ElementAt(1);
+                if (ttt.Count() > 2)
+                    infoo.RelationShip3 = ttt.ElementAt(2);
 
-            return View(infoo);
+                return View(infoo);
+            }
+            else
+            {
+                
+                return View("ListNewPerson");
+               
+            }
+           
         }
 
         public ActionResult ListCheckedPerson()
